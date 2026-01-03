c6b2c245e6efd9ba2f1b9600f842974ba4d4ba5f
Ana <analaudamasco@gmail.com>
2026-01-02 22:31:24 -0300

    agrego color segun el tipo al borde

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -2,10 +2,35 @@ import './../App.css';
 
 function PokemonCard({ pokemon }) {
 
+const typeColors = {
+  fire: "#F08030",
+  water: "#6890F0",
+  grass: "#78C850",
+  electric: "#F8D030",
+  psychic: "#F85888",
+  ice: "#98D8D8",
+  dragon: "#7038F8",
+  dark: "#705848",
+  fairy: "#EE99AC",
+  poison: "#A040A0",
+  flying: "#A890F0",
+  bug: "#A8B820",
+  normal: "#A8A878",
+  ground: "#E0C068",
+  rock: "#B8A038",
+  steel: "#B8B8D0",
+  fighting: "#C03028",
+  ghost: "#705898",
+};
+
+const mainType = pokemon.types[0].type.name;
+const typeColor = typeColors[mainType] || "#e2e2d1ff";
+
+
 console.log(pokemon);
 
   return (
-    <div className="card">
+    <div className="card" style={{ borderColor: typeColor }}>
     <img src={pokemon.sprites.front_default} alt={pokemon.name}/>   
       <h3>{pokemon.name}</h3>
       <p>Altura: {pokemon.height}</p>
